c31c3f2147acf193c98373b83ed873ca07d46aef
Patrick Khouri <patrick.darya@gmail.com>
2022-04-26 12:39:34 -0300

    setup router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,13 @@
 const express = require('express')
 const { restart } = require('nodemon');
 const app = express();
-let { people } = require('./data');
+
+const people = require('./routes/people')
 
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-
-app.get('/api/people', (req,res) => {
-  res.status(200).json({success: true, data: people})
-})
-
-app.post('api/people', (req, res) => {
-  const {name} = req.body
-  if (!name) {
-    return res.status(400).json({success: false, msg: 'Please provide a name'})
-  } else {
-    return res.status(201).json({success: true, person: name, msg: `Successfully added ${name}`})
-  }
-})
+app.use('/api/people', people)
 
 app.post('/login', (req, res) => {
   const {name} = req.body
@@ -27,35 +16,6 @@ app.post('/login', (req, res) => {
   } else {
     return res.status(401).send('Please provide login credentials')
   }
-})
-
-app.put('/api/people/:id', (req, res) => { 
-  const {id} = req.params
-  const {name} = req.body
-
-
-  const person = people.find((person) => person.id === Number(id))
-  if (!person) {
-    return res.status(404).json({success: false, msg: `No person with id ${id}`})
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name
-    }
-    return person
-  })
-  res.status(200).json({success: true, data: newPeople})
-})
-
-app.put('/api/people/:id', (req, res) => { 
-  const {id} = req.params
-
-  const person = people.find((person) => person.id === Number(id))
-  if (!person) {
-    return res.status(404).json({success: false, msg: `No person with id ${id}`})
-  }
-  const newPeople = people.filter((person) => person.id !== Number(id))
-  res.status(200).json({success: true, data: newPeople})
 })
 
 app.listen(5000, () => {
